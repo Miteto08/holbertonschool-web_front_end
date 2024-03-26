af7f2dd258d6bef1aba5757c45fd3c894050bbc8
Miteto08 <julien.lechevalier@shortways.com>
2024-03-26 08:33:14 +0100

    task 5 - dom style modification

---
+++ Javascript_advanced/5-mode.js
@@ -0,0 +1,11 @@
+function changeMode(size, weight, transform, background, color) {
+    document.documentElement.style['font-size'] = size
+    document.documentElement.style['font-weight'] = weight
+    document.documentElement.style['text-transform'] = transform
+    document.documentElement.style['background-color'] = background
+    document.documentElement.style['color'] = color
+}
+
+function main() {
+    var spooky = changeMode(9, bold,uppercase)
+}
