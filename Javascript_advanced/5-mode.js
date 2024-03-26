1c8a4a20dc26965ac1b6328a9d35a243cbed8e09
Miteto08 <julien.lechevalier@shortways.com>
2024-03-26 09:53:48 +0100

    task 5 - add html inner

--- Javascript_advanced/5-mode.js
+++ Javascript_advanced/5-mode.js
@@ -7,5 +7,12 @@ function changeMode(size, weight, transform, background, color) {
 }
 
 function main() {
-    var spooky = changeMode(9, bold,uppercase)
+    var spooky = changeMode(9, bold, uppercase, pink, green)
+    var darkMode = changeMode(12, bold, capitalize, black, white)
+    var screamMode = changeMode(12, normal, lowercase, white, black)
+    document.querySelector("body").innerHTML = `
+    <p>Welcome Holberton!</p>
+    <button id= >Spooky</button>
+    `
+    
 }
