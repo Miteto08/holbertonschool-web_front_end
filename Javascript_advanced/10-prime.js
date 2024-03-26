eef851672f38ca942f0c7ebd680daa0e878df63d
Miteto08 <julien.lechevalier@shortways.com>
2024-03-26 14:32:22 +0100

    task 10 - Execution stack & timing execution + indentation correction

--- Javascript_advanced/10-prime.js
+++ Javascript_advanced/10-prime.js
@@ -2,7 +2,7 @@ function countPrimeNumbers() {
     let isPrime = true
     let numberCount = 0
 
-    for (let i = 1; i <= 100; i++) {
+    for (let i = 2; i <= 100; i++) {
         isPrime = true
         let sqrt = Math.round(Math.sqrt(i))
         for (let div = 2; div <= sqrt; div++) {
@@ -19,7 +19,9 @@ function countPrimeNumbers() {
 }
 
 let startExecution = window.performance.now()
-countPrimeNumbers()
+for (let i = 0; i < 100; i++) {
+    countPrimeNumbers()
+}
 let stopExecution = window.performance.now()
 
-console.log("Execution time of printing countPrimeNumbers was " + (stopExecution - startExecution) + " milliseconds.")
+console.log("Execution time of calculating prime numbers 100 times was " + (stopExecution - startExecution) + " milliseconds.")
